11ef7e80d74269763bf47ca084116b550f1bf57b
Rafael Soje <rafaelsoje@gmail.com>
2021-09-17 22:45:34 -0300

    Create routes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,53 @@ use app\http\Controllers\WarningController;
 Route::get('/ping', function(){
     return ['pong' => true ];
 });
+
+Route::get('/401', [AuthController::class, 'unauthorized'])->name('login');
+
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/register', [AuthController::class, 'register']);
+
+Route::middleware('auth:api')->group(function(){
+    Route::post('/auth/validate', [AuthController::class, 'validateToken']);
+    Route::post('auth/logout', [AuthController::class, 'logout']);
+
+    //Mural de avisos
+    Route::get('/walls', [WallController::class, 'getAll']);
+    Route::post('/wall/{id}/like', [WallController::class, 'like']);
+
+    //Documentos
+    Route::get('/docs', [DocController::class, 'getAll']);
+
+    //Livro de ocorrências
+    Route::get('/warnings', [WallController::class, 'getMyWarnings']);
+    Route::post('/warning', [WallController::class, 'setWarning']);
+    Route::post('/warning/file', [WallController::class, 'addWarningFile']);
+
+    //Boletos
+    Route::get('/billets', [BilletController::class, 'getAll']);
+
+    //Achados e perdidos
+    Route::get('/foundandlost', [FoundAndLostController::class, 'getall']);
+    Route::post('/foundandlost', [FoundAndLostController::class, 'insert']);
+    Route::put('/foundandlost/{id}', [FoundAndLostController::class, 'update']);
+
+    //Unidade
+    Route::get('/unit/{id}', [UnitController::class, 'getInfo']);
+    Route::post('/unit/{id}/addperson', [UnitController::class, 'addPerson']);
+    Route::post('/unit/{id}/addvehicle', [UnitController::class, 'addVehicle']);
+    Route::post('/unit/{id}/addpet', [UnitController::class, 'addPet']);
+    Route::post('/unit/{id}/removeperson', [UnitController::class, 'removePerson']);
+    Route::post('/unit/{id}/removevehicle', [UnitController::class, 'removeVehicle']);
+    Route::post('/unit/{id}/removepet', [UnitController::class, 'removePet']);
+
+    //Reservas
+    Route::get('/reservations', [ReservationController::class, 'getReservations']);
+    Route::post('/reservation/{id}', [ReservationController::class, 'setReservation']);
+
+    Route::get('/reservation/{id}/disabledates', [ReservationController::class, 'getDisabledates']);
+    Route::get('/reservation/{id}/times', [ReservationController::class, 'getTimes']);
+
+    Route::get('/myreservations', [ReservationController::class, 'getMyReservations']);
+    Route::delete('/myreservation/{id}', [ReservationController::class, 'delMyReservation']);
+    
+});
